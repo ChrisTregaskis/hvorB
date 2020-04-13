075395d23bf1c0ffcc1561a5c218584fefbd06a3
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-13 15:02:14 +0100

    created getAllUsers method grabbing users and passing through UserEntity

--- src/Classes/Models/UserModel.php
+++ src/Classes/Models/UserModel.php
@@ -14,4 +14,16 @@ class UserModel
     {
         $this->db = $db;
     }
+
+    /** Gets all the users from DB and sorted alphabetically by company
+     *
+     * @return array
+     */
+    public function getAllUsers()
+    {
+        $query = $this->db->prepare('SELECT `id`, `fname`, `sname`, `fullName`, `knownCompany`, `KnownEmail`, `companyAPIResult`, `matchPrevious`, `matchCurrent` FROM `users_test` ORDER BY `knownCompany`;');
+        $query->setFetchMode(\PDO::FETCH_CLASS, 'WBApp\Entities\UserEntity');
+        $query->execute();
+        return $query->fetchAll();
+    }
 }
